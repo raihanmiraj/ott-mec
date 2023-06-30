23a772b39dece182c40d1ff527dc61348d40e450
raihanmiraj <rsnmiraj@gmail.com>
2023-06-30 12:24:27 +0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-// import Providers from '@/provider'
 import './globals.css'
 import { Inter } from 'next/font/google'
-// import AuthContextProvider from '@/provider/AuthContextProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,12 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Providers> */}
-      {/* <AuthContextProvider> */}
       <body className={inter.className}>{children}</body>
-      {/* </AuthContextProvider> */}
-      {/* </Providers> */}
-     
     </html>
   )
 }
